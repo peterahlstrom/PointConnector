12fa3038ba73483053d8e22e25ac0d8e17d6daea
Peter Ahlström <ahlstrom.peter@gmail.com>
2014-07-22 11:23:23 +0200

    #3

--- point_connector_dialog.py
+++ point_connector_dialog.py
@@ -6,7 +6,6 @@
  Creating lines between points following a from-to list.
                              -------------------
         begin                : 2014-07-15
-        git sha              : $Format:%H$
         copyright            : (C) 2014 by Peter Ahlstrom
         email                : ahlstrom (dot) peter (at) gmail (dot) com
  ***************************************************************************/
@@ -23,7 +22,7 @@
 
 import os
 
-from PyQt4 import QtGui, uic
+from PyQt4 import QtGui, uic, QtCore
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'point_connector_dialog_base.ui'))
@@ -31,29 +30,27 @@ FORM_CLASS, _ = uic.loadUiType(os.path.join(
 
 class PointConnectorDialog(QtGui.QDialog, FORM_CLASS):
     def __init__(self, parent=None):
-        """Constructor."""
+        #Constructor
         super(PointConnectorDialog, self).__init__(parent)
-        # Set up the user interface from Designer.
-        # After setupUI you can access any designer object by doing
-        # self.<objectname>, and you can use autoconnect slots - see
-        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
-        # #widgets-and-dialogs-with-auto-connect
+        self.settings = QtCore.QSettings("petahl", "PointConnector")
         self.setupUi(self)
         self.browsePointButton.clicked.connect(self.browsePointButton_clicked)            
         self.browseCsvButton.clicked.connect(self.browseCsvButton_clicked)
         
-
-# Browse Point button
-
+    # Browse Point button
     def browsePointButton_clicked(self):
-        pointFileName = QtGui.QFileDialog.getOpenFileName(self, 'Open File', os.getenv('HOME'))
+        lastShapeDir = self.settings.value("lastShapeDir", ".")
+        pointFileName = QtGui.QFileDialog.getOpenFileName(self, 'Open File', lastShapeDir, 'ESRI Shape files (*.shp)')
         self.pointPathLineEdit.setText(pointFileName)
         self.pointFileName = pointFileName
+        (shpDir, shpFile) = os.path.split(pointFileName)
+        self.settings.setValue("lastShapeDir", shpDir)
 
-
-# Browse CSV button
-
+    # Browse CSV button
     def browseCsvButton_clicked(self):
-        csvFileName = QtGui.QFileDialog.getOpenFileName(self, 'Open File', os.getenv('HOME'))
+        lastCsvDir = self.settings.value("lastCsvDir", ".")
+        csvFileName = QtGui.QFileDialog.getOpenFileName(self, 'Open File', lastCsvDir, 'Text files (*.csv *.txt)')
         self.csvPathLineEdit.setText(csvFileName)
         self.csvFileName = csvFileName
+        (csvDir, csvFile) = os.path.split(csvFileName)
+        self.settings.setValue("lastCsvDir", csvDir)
